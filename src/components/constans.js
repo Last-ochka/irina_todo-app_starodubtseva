ddeb897eda09fc9c971359da31a0a40dc08dbf62
Irina Starodubtseva <st.irina.fedorovna@gmail.com>
2022-07-06 10:29:09 +0300

    new constans

--- src/components/constans.js
+++ src/components/constans.js
@@ -1,3 +1,6 @@
 export const HEADER_NAME = 'TODO';
 export const FOOTER_NAME = '2022';
 export const PAGE_SIZE = 8;
+export const SORTING_ACTIVE = 'Active';
+export const SORTING_ALL = 'All';
+export const SORTING_COMPLETED = 'Completed';
